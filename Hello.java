f25fe39270ae6f605efd723ae343da5f946c0fd4
JohnJohan <142964327+JohnJohan@users.noreply.github.com>
2023-09-05 16:39:41 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello(
 public static void main(String[] args){
-System.out.print("Hello, Software Engineering Course!");
-System.out.print("My name is San Juan, John Johan!");
+System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
